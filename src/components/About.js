55f92ebd77c3124edb32829558ec4f4dd14cc157
Saket Kothari <saketkothari722@gmail.com>
2023-01-24 14:29:58 +0530

    Implementing Class Based Components

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,43 @@
-const About = () => {
-  return (
-    <div className="about-container">
-      <h1 className="about-title">About Us Page</h1>
-      <p className="about-text">Finding the Path 🚀</p>
-    </div>
-  );
-};
+import ProfileFunctionalComponet from './Profile';
+import { Component } from 'react';
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    // console.log("Parent - constructor");
+  }
+  componentDidMount() {
+    // Best place to make an Api call
+    // console.log("Parent - componentDidMount");
+  }
+  render() {
+    // console.log("Parent - render");
+    return (
+      <div className="about-container">
+        <h1 className="about-title">About Us Page</h1>
+        <p className="about-text">Class Based Component 🚀</p>
+        <ProfileFunctionalComponet />
+      </div>
+    );
+  }
+}
 
 export default About;
+
+/**
+ *
+ * Parent Constructor
+ * Parent render
+ *    First Child constructor
+ *    First Child render
+ *    Second Child constructor
+ *    Second Child render
+ *
+ *    DOM UPDATED for children
+ *
+ *    first Child componentDidMount
+ *    Second Child componentDid
+ *  Parent componentDidMount
+ *
+ */
