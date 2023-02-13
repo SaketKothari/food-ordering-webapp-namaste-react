ce1b372099392aa7c45d9ec761fc197693b6be69
Saket Kothari <saketkothari722@gmail.com>
2023-02-13 09:39:38 +0530

    Adding name to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import Logo from '../assets/img/foodvilla.png';
 import { Link } from 'react-router-dom';
 import useOnline from '../utils/useOnline';
+import UserContext from '../utils/context/UserContext';
 
 const loggedInUser = () => {
   // API call to check authentication
@@ -17,6 +18,7 @@ const Title = () => (
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const isOnline = useOnline();
+  const { user } = useContext(UserContext);
 
   return (
     <div className="flex justify-between bg-pink-50 shadow-lg sm:bg-blue-50 md:bg-yellow-50">
@@ -41,6 +43,7 @@ const Header = () => {
         </ul>
       </div>
       <h1>{isOnline ? '✅' : '🔴'}</h1>
+      <span className="p-10 font-bold text-red-900">{user.name}</span>
       {isLoggedIn ? (
         <button onClick={() => setIsLoggedIn(false)}>Logout</button>
       ) : (
